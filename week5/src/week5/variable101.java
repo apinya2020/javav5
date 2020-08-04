ad6938764316a128aa4f1586cf581cbd12af145e
apinya2020 <apinya.jitsampita@gmail.com>
2020-08-04 17:55:31 +0700

    feature Version 1.1

--- week5/src/week5/variable101.java
+++ week5/src/week5/variable101.java
@@ -4,11 +4,13 @@ public class variable101 {
 	String freinds4 = "Jenny";//instance variable out of method main
 	static int count = 4;//static variable is Global variable
 	final String song = "How you like that";//Final variable
+	static String version ="V1.1";
 	
 	public static void main(String[] args) {//main method
 		// TODO Auto-generated method stub
 		variable101 member = new variable101();
 		String friends1 = "Lisa";//local variable
+		System.out.println("Version : "+ version);
 		System.out.println("Member of Blackping have : " + count);
 		System.out.println("first member of Blackpink name is : " + friends1);
 		System.out.println("===========");
